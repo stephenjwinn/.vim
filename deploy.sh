c2db1aaad89b95722f2ae91ea57b88074d1a5f3f
Stephen Winn <stephenwinn@rockventures.com>
2015-10-22 13:51:20 +0000

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Deploy vim
 
+# Symlink Me, Bruh
 ln -sf ~/.vim/.vimrc ~/.vimrc
-apt-get install silversearcher-ag
+
+# Get Dat Silver Searcher
+apt-get -q -y install silversearcher-ag
+
+# Reinstall Vundle
+cd bundle
+git clone https://github.com/VundleVim/Vundle.vim.git Vundle.vim
+
+# Install the goodies.
+vim +PluginInstall +qall
